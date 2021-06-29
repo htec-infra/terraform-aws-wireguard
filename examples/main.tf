e41673f3b3a24f638b766b650a7558ea26a5292e
Jovica Zlatanovic <jovica.zlatanovic@htecgroup.com>
2021-06-29 16:46:40 +0200

    Documentation update

--- examples/main.tf
+++ examples/main.tf
@@ -1,14 +1,14 @@
 module "vpn" {
   source = "../"
 
-  # General settings, used for resource naming and tags
+  # General settings, used for resource identifiers and tags
   namespace   = "Project"
   env_code    = "ops"
   environment = "Operations"
   cost_center = "Infrastructure"
 
   # domain configuration:
-  # fqdn = subspace_subdomain.root_domain
+  # fqdn = subspace_subdomain.root_domain = vpn.example.com
   subspace_subdomain = "vpn"
   root_domain        = "example.com"
 
@@ -16,6 +16,6 @@ module "vpn" {
   subnet_ids = ["subnet-public12345"]
 
   # wireguard internal network CIDR.
-  # NOTE: DO NOT overlap Wireguard network with the AWS VPC network
+  # NOTE: DO NOT overlap Wireguard network CIDR with the AWS VPC network CIDR
   wg_ipv4_network = "10.250.0.0/24"
 }
